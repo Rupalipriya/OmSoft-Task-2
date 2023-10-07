84e4d44f79e691a964676dad176487df705a0a41
Rupalipriya <rupalipriya6991@gmail.com>
2023-10-07 11:28:34 +0530

    paymnet gateway integration

---
+++ src/main/java/com/sample/razor/pay/RazorPayApp.java
@@ -0,0 +1,13 @@
+package com.sample.razor.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorPayApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RazorPayApp.class, args);
+	}
+
+}
